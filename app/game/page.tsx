7dadb5a045b5fc34f56827a6f028ff6a42ff18b0
CodeBloodedMama <elisabethbinzer@hotmail.com>
2025-09-07 19:17:35 +0200

    .

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 import React from "react";
-import GameFrame from "../GameFrame";
+import GameFrame from "@/app/game/GameFrame";
 import GameBox from "@/app/game/GameBox";
 
-
-
-
-export default function CVPage() {
+export default function CVgame() {
   return (
     <div className="min-h-screen w-full bg-black text-white flex ">
        
           <div className="w-full h-[180vh] flex flex-col items-center justify-center mt-40">
             <GameFrame  className="w-full h-full max-w-none rounded-xl ">
+              
               <GameBox />
 
             </GameFrame>
