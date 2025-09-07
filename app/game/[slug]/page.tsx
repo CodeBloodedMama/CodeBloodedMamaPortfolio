9357e5aecc02d9097c966a9239287e88234d8b07
CodeBloodedMama <elisabethbinzer@hotmail.com>
2025-09-07 18:55:15 +0200

    .,

--- app/game/[slug]/page.tsx
+++ app/game/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from "react";
 import GameFrame from "../GameFrame";
-import GameBox from "../GameBox";
+import GameBox from "@/app/game/GameBox";
 
 
 
@@ -13,6 +13,7 @@ export default function CVPage() {
           <div className="w-full h-[180vh] flex flex-col items-center justify-center mt-40">
             <GameFrame  className="w-full h-full max-w-none rounded-xl ">
               <GameBox />
+
             </GameFrame>
             <p className="mt-2 text-xs text-white/60 text-center w-full">
               Tip: Roter evt. din telefon for større spilleflade
